3289a4c7958be588db128aab2e9d455813b315d3
Jeina <minn@minjeong-ui-MacBook-Pro.local>
2019-02-10 23:55:20 +0900

    Feat: Day 2

--- Day2_Operators.py
+++ Day2_Operators.py
@@ -1,3 +1,7 @@
+# Hackerrank 30 Days of code Challenge
+# https://www.hackerrank.com/domains/tutorials/30-days-of-code
+# Day 2
+
 # Complete the solve function below.
 def solve(meal_cost, tip_percent, tax_percent):
     print(int(round((meal_cost * (1 + (tip_percent + tax_percent) / 100)))))
