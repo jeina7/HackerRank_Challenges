f2db52577a6ef5e3a259ff5aa0e80518f7a382cf
Jeina <minn@minjeong-ui-MacBook-Pro.local>
2019-02-13 23:45:45 +0900

    Feat: Day 5

---
+++ Day5_Loops.py
@@ -0,0 +1,8 @@
+# Hackerrank 30 Days of code Challenge
+# https://www.hackerrank.com/domains/tutorials/30-days-of-code
+# Day 5
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, 11):
+        print('{} x {} = {}'.format(n, i, i * n))
