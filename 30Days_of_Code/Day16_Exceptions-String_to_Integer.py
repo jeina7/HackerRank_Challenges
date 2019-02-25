23f4d5ccc023bac88148e4c20d5290b4095ab4ae
jeina7 <jane1231001@gmail.com>
2019-02-25 18:46:49 +0900

    Feat: Day 16

---
+++ 30Days_of_Code/Day16_Exceptions-String_to_Integer.py
@@ -0,0 +1,12 @@
+# Hackerrank 30 Days of code Challenge
+# https://www.hackerrank.com/domains/tutorials/30-days-of-code
+# Day 16
+
+import sys
+
+S = input().strip()
+try:
+    S = int(S)
+    print(S)
+except:
+    print("Bad String")
